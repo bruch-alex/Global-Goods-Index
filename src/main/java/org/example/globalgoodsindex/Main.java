19669482659f12e6fb87235d255244934f2b1d81
bruch-alex <alex.bruch@tutanota.com>
2024-11-20 14:04:40 +0100

    rename files according to javaFX convention

--- src/main/java/org/example/globalgoodsindex/Main.java
+++ src/main/java/org/example/globalgoodsindex/Main.java
@@ -1,15 +1,9 @@
 package org.example.globalgoodsindex;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.stage.Stage;
-import org.example.globalgoodsindex.core.models.Entry;
 
 import java.io.IOException;
 
@@ -20,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
