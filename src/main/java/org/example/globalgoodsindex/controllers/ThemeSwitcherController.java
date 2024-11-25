23166fa77b9902baefcbcfbcd482b8757044f311
bruch-alex <alex.bruch@tutanota.com>
2024-11-25 14:26:14 +0100

    change layout, add icons

---
+++ src/main/java/org/example/globalgoodsindex/controllers/ThemeSwitcherController.java
@@ -0,0 +1,14 @@
+package org.example.globalgoodsindex.controllers;
+
+import javafx.fxml.FXML;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+public class ThemeSwitcherController {
+    @FXML
+    private FontIcon icon;
+
+    // onClick method(){
+    // change theme
+    // change icon
+    // }
+}
