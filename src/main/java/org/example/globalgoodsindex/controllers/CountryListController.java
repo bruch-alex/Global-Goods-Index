9ff8f573f8c338d885417d215bf05797b6558ad2
bruch-alex <alex.bruch@tutanota.com>
2024-11-27 11:18:21 +0100

    add search bar for countries and products

--- src/main/java/org/example/globalgoodsindex/controllers/CountryListController.java
+++ src/main/java/org/example/globalgoodsindex/controllers/CountryListController.java
@@ -2,10 +2,15 @@ package org.example.globalgoodsindex.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import org.example.globalgoodsindex.Main;
 import org.example.globalgoodsindex.core.models.Entry;
+import org.example.globalgoodsindex.core.models.Salaries;
+
+import java.util.List;
 
 
 public class CountryListController {
@@ -17,14 +22,30 @@ public class CountryListController {
     private Label labelCountries;
 
     @FXML
+    private TextField countrySearchField;
+
+    @FXML
     public void initialize() {
         bindStrings();
         countryList.setItems(FXCollections.observableArrayList(Main.dataHandler.getSalaries()));
         countryList.setCellFactory(_ -> new CheckBoxFactory());
+
+        countrySearchField.textProperty().addListener(((observableValue, oldValue, newValue) -> {
+            List<Salaries> filteredData = Main.dataHandler.getSalaries().stream()
+                    .filter(item -> I18N.get(item.getName()).toLowerCase().contains(newValue.toLowerCase()))
+                    .toList();
+
+            countryList.getItems().clear();
+            countryList.setItems(FXCollections.observableArrayList(filteredData));
+            countryList.setCellFactory(_ -> new CheckBoxFactory());
+        }));
+
+
     }
 
     @FXML
     private void bindStrings(){
         labelCountries.textProperty().bind(I18N.createStringBinding("countries"));
+        countrySearchField.promptTextProperty().bind(I18N.createStringBinding("search"));
     }
 }
--- src/main/java/org/example/globalgoodsindex/controllers/ProductListController.java
+++ src/main/java/org/example/globalgoodsindex/controllers/ProductListController.java
@@ -4,8 +4,12 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import org.example.globalgoodsindex.Main;
 import org.example.globalgoodsindex.core.models.Entry;
+import org.example.globalgoodsindex.core.models.Product;
+
+import java.util.List;
 
 public class ProductListController {
     @FXML
@@ -15,14 +19,28 @@ public class ProductListController {
     private Label productsLabel;
 
     @FXML
+    private TextField productSearchField;
+
+    @FXML
     public void initialize() {
         bindStrings();
         productList.setItems(FXCollections.observableArrayList(Main.dataHandler.getProducts()));
         productList.setCellFactory(_ -> new CheckBoxFactory());
+
+        productSearchField.textProperty().addListener(((observableValue, oldValue, newValue) -> {
+            List<Product> filteredData = Main.dataHandler.getProducts().stream()
+                    .filter(item -> I18N.get(item.getName()).toLowerCase().contains(newValue.toLowerCase()))
+                    .toList();
+
+            productList.getItems().clear();
+            productList.setItems(FXCollections.observableArrayList(filteredData));
+            productList.setCellFactory(_ -> new CheckBoxFactory());
+        }));
     }
 
     @FXML
     private void bindStrings() {
         productsLabel.textProperty().bind(I18N.createStringBinding("products"));
+        productSearchField.promptTextProperty().bind(I18N.createStringBinding("search"));
     }
 }
