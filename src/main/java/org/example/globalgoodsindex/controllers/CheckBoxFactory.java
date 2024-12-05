253a44420620609febc010384d1fd143b53aeb16
bruch-alex <alex.bruch@tutanota.com>
2024-12-05 11:14:41 +0100

    cleanup factory

--- src/main/java/org/example/globalgoodsindex/controllers/CheckBoxFactory.java
+++ src/main/java/org/example/globalgoodsindex/controllers/CheckBoxFactory.java
@@ -7,14 +7,18 @@ import org.example.globalgoodsindex.models.Product;
 import org.example.globalgoodsindex.services.L10N;
 
 public class CheckBoxFactory extends ListCell<Entry> {
+
     @Override
     protected void updateItem(Entry entry, boolean empty) {
+
         super.updateItem(entry, empty);
-        //System.out.println("updateItem called for: " + (entry != null ? entry.getName() : "empty"));
+
         if (empty || entry == null) {
             setGraphic(null);
         } else {
+
             CheckBox checkBox = new CheckBox();
+
             if (entry instanceof Product) {
                 checkBox.textProperty().bind(L10N.createShortNameBinding((Product) entry));
             } else {
@@ -22,14 +26,6 @@ public class CheckBoxFactory extends ListCell<Entry> {
             }
 
             checkBox.selectedProperty().bindBidirectional(entry.selectedProperty());
-
-//            // Listener for debugging only
-//            checkBox.selectedProperty().addListener((_, oldValue, newValue) -> {
-//                if (newValue || oldValue) {
-//                    System.out.println("Changed value for checkbox " + checkBox.getText() + " from " + oldValue + " to " + newValue);
-//                    System.out.println("Value for instance " + entry.getName() + " is " + entry.isSelected());
-//                }
-//            });
 
             setGraphic(checkBox);
         }
